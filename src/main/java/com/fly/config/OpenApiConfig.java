7835d9731218fa08497f83136a2ebc362fb2796d
Flying <2127166541@qq.com>
2023-09-12 20:50:36 +0800

    update:添加API文档

---
+++ src/main/java/com/fly/config/OpenApiConfig.java
@@ -0,0 +1,38 @@
+package com.fly.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author Milk
+ * @Date 2023/9/12 19:39
+ */
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI insightBlogApi() {
+        return new OpenAPI().info(new Info()
+                .title("Memory Blog API")
+                .description("Spring Boot Application")
+                .contact(new Contact().name("FlyBullet"))
+                .version("1.0.0 GA"));
+    }
+
+    @Bean
+    @SuppressWarnings("all")
+    public GroupedOpenApi defaultApiGroup() {
+        return GroupedOpenApi.builder()
+                .packagesToScan("com.fly.blog.controller")
+                .group("default")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+}
+
+
